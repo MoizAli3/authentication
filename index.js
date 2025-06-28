78d0143e7ab7d1eb04f73c8cdd1fe08a76ae3fac
Moiz Ali <moizk5590@gmail.com>
2025-06-29 00:06:04 +0500

    some changes

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ connectMongoDb(process.env.DATABASE_URI);
 
 const allowedOrigins = process.env.CLIENT_URL;
 
-app.use(cors({ origin: "https://frontend-authentication-ce9p.onrender.com" }));
-
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (
+        !origin || "https://frontend-authentication-ce9p.onrender.com".includes(origin)
+      ) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS!"));
