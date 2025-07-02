467dd4c2f8ba5a7d70e6c20f4f879fd781a88fa0
Moiz Ali <moizk5590@gmail.com>
2025-07-02 20:49:36 +0500

    remove protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import SignUp from "./pages/SignUp";
-import Layout from "./Layout";
+// import Layout from "./Layout";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import ProtectedRoute from "./components/ProtectedRoute";
+
 
 function App() {
   return (
